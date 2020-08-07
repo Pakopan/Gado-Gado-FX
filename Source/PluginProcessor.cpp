513362c12085a56241ad1f213aa9a5cc92fcfbb3
Ervan Andi <ervan.andi45@gmail.com>
2020-08-07 20:30:55 +0700

    added xml save and load parameter feature

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -269,15 +269,18 @@ juce::AudioProcessorEditor* GadoGadoFXAudioProcessor::createEditor()
 //==============================================================================
 void GadoGadoFXAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = parameters.valueTreeState.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void GadoGadoFXAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+
+    if (xmlState.get() != nullptr)
+        if (xmlState->hasTagName(parameters.valueTreeState.state.getType()))
+            parameters.valueTreeState.replaceState(juce::ValueTree::fromXml(*xmlState));
 }
 
 //==============================================================================
